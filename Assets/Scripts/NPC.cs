b748f473ab641761aeed8b7344b674aceeddc9e0
ThunderAwesome <troyjsanders@gmail.com>
2019-02-16 13:44:32 -0600

    Basic navigation

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+public class NPC : MonoBehaviour
+{
+    public Transform[] goals;
+
+    // TJS: keeps track of which goal is the next target
+    private int goalIndex = 0;
+
+    private NavMeshAgent m_agent;
+    private Rigidbody m_rigidbody;
+
+    private void Start()
+    {
+        Initialize();
+
+        if (goals == null || goals.Length <= 0)
+        {
+            Debug.LogError($"<color=white>{nameof(NPC)}</color>: Array <color=red>{nameof(goals)}</color> is null or does not have any values. Please assign a Transform value in the inspector.");
+        }
+        else
+        {
+            StartCoroutine(NavigateToGoalTarget());
+        }        
+    }
+
+    private void Initialize()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void SetAgentDestinationGoal(Transform goalTransform)
+    {
+        m_agent.destination = goalTransform.position;
+    }
+
+    private IEnumerator NavigateToGoalTarget()
+    {
+        if(goals.Length > 0)
+        {
+            while (true)
+            {
+                yield return null;
+                SetAgentDestinationGoal(goals[goalIndex]);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Waypoint")
+        {
+            if(other.transform == goals[goalIndex])
+            {
+                IncrementGoalIndex();
+            }
+        }
+    }
+
+    private void IncrementGoalIndex()
+    {
+        goalIndex = (goalIndex >= goals.Length-1) ? 0 : ++goalIndex;
+        Debug.Log($"{nameof(goalIndex)}: {goalIndex}");
+    }
+}
