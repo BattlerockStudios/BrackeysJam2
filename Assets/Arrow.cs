a2775a6f088ee32e9e4664dc4385afaed34183a9
GingerLoaf <the_ds_guy@yahoo.com>
2019-02-17 06:26:14 -0600

    Adding basic core mechanics

---
+++ Assets/Arrow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.GetComponent<Rigidbody>().isKinematic = true;
+
+        transform.SetParent(collision.transform, worldPositionStays: true);
+        transform.localScale = Vector3.one;
+        enabled = false;
+    }
+
+}
---
+++ Assets/cupidcontroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class cupidcontroller : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject m_arrowPrefab = null;
+
+    [SerializeField]
+    private Camera m_camera = null;
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = m_camera.ScreenPointToRay(Input.mousePosition);
+            var copy = Instantiate(m_arrowPrefab);
+            copy.transform.forward = ray.direction;
+            copy.transform.position = transform.position;
+            copy.GetComponent<Rigidbody>().velocity = ray.direction * 50;
+        }
+    }
+}
