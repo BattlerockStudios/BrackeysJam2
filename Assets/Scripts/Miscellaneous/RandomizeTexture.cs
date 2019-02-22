9209f6c0da7e43f9bdb80883a830a328d1501e73
Troy Sanders <troyjsanders@gmail.com>
2019-02-22 08:25:21 -0600

    Set up NPCs to use random textures for variation.

---
+++ Assets/Scripts/Miscellaneous/RandomizeTexture.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class RandomizeTexture : MonoBehaviour
+{
+    public Texture2D[] textureArray;
+
+    private Renderer m_renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_renderer = GetComponent<Renderer>();
+        m_renderer.material.SetTexture("_MainTex", textureArray[Random.Range(0, textureArray.Length)]);
+    }
+}
